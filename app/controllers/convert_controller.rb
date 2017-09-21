ab0d7695634bb5f939becaf75aaa62ac47725329
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2017-09-21 14:01:12 +0100

    second round version

--- app/controllers/convert_controller.rb
+++ app/controllers/convert_controller.rb
@@ -1,10 +1,24 @@
 class ConvertController < ApplicationController
-    
+
     def index
     end
-    
+
+    #round one version
     def celsius
         @celsius_temp = (params[:fahrenheit].to_i-32)*0.5556
         return @celsius_temp
     end
+
+    #round two version
+    def temp
+      @output = (params[:output])
+        if ( @output == "C")
+            @temp = (params[:value].to_i-32)*0.5556
+        elsif ( @output == "F")
+            @temp = ((params[:value].to_i*9/5) +32)
+        else
+            @output = "you need to pick the temperature scale"
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'convert/index'
-  get 'convert/celsius'
+  # convert/celsius is in round one
+ # get 'convert/celsius'
+ # convert/temp is in round two
+ get 'convert/temp'
+  
   root 'convert#index'
 end
